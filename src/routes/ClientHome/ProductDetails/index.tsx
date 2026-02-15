09ccc053e43e5b4900c7c89b957727771df6c17c
Victor Rodrigues <victor.rodrigues@itbeta2.com.br>
2026-02-15 20:51:46 -0300

    links, catalog, product-details

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -3,7 +3,7 @@ import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import * as productService from "../../../services/product-service";
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function ProductDetails() {
 
@@ -18,10 +18,12 @@ export default function ProductDetails() {
                     product &&
                     <ProductDetailsCard product={product} />
                 }
-                
+
                 <div className="dsc-btn-page-container">
                     <ButtonPrimary text="Comprar" />
-                    <ButtonInverse text="Início" />
+                    <Link to={"/"}>
+                        <ButtonInverse text="Início" />
+                    </Link>
                 </div>
             </section>
         </main>
