ccf08a1756f547c0222b0ed63936dd4358fe9da0
Victor Rodrigues <victor.rodrigues@itbeta2.com.br>
2026-04-15 00:44:35 -0300

    Place order

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -2,14 +2,17 @@ import { useContext, useState } from 'react';
 import './styles.css';
 import * as cartService from "../../../services/cart-service";
 import { type OrderDTO } from '../../../models/order';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { ContextCartCount } from '../../../utils/context-cart';
+import * as orderService from '../../../services/order-service';
 
 export default function Cart() {
 
     const [cart, setCart] = useState<OrderDTO>(cartService.getCart());
 
     const { setContextCartCount } = useContext(ContextCartCount);
+
+    const navigate = useNavigate();
 
     function handleClearClick() {
         cartService.clearCart();
@@ -30,6 +33,15 @@ export default function Cart() {
         const newCart = cartService.getCart();
         setCart(newCart);
         setContextCartCount(newCart.items.length);
+    }
+
+    function handlePlaceOrderClick() {
+        orderService.placeOrderRequest(cart)
+            .then(response => {
+                cartService.clearCart();
+                setContextCartCount(0);
+                navigate(`/confirmation/${response.data.id}`);
+            });
     }
 
     return (
@@ -74,7 +86,7 @@ export default function Cart() {
                 }
 
                 <div className="dsc-btn-page-container">
-                    <div className="dsc-btn dsc-btn-blue">
+                    <div onClick={handlePlaceOrderClick} className="dsc-btn dsc-btn-blue">
                         Finalizar pedido
                     </div>
                     <Link to="/catalog" >
