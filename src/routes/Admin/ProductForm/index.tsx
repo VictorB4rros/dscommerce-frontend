0f2cb53faecd919d58c7a9b48cb2f268356f02e1
Victor Rodrigues <victor.rodrigues@itbeta2.com.br>
2026-05-21 00:16:17 -0300

    Forms validate

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -21,11 +21,15 @@ export default function ProductForm() {
             placeholder: "Nome",
         },
         price: {
-            value: "",
+            value: 100,
             id: "price",
             name: "price",
             type: "number",
             placeholder: "Preço",
+            validation: function(value: any) {
+                return Number(value) > 0;
+            },
+            message: "Favor informar um valor positivo"
         },
         imgUrl: {
             value: "",
@@ -37,6 +41,10 @@ export default function ProductForm() {
     });
 
     useEffect(() => {
+
+        const obj = forms.validate(formData, "price");
+        console.log(obj);
+
         if (isEditing) {
             productService.findById(Number(params.productId))
                 .then(response => {
