c55fe15344afd48f398a4f32c49b81ef3fb1056e
Victor Rodrigues <victor.rodrigues@itbeta2.com.br>
2026-05-20 23:27:48 -0300

    Forms to value

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,3 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function update(inputs: any, name: string, newValue: any) {
     return { ...inputs, [name]: { ...inputs[name], value: newValue } };
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function toValues(inputs: any) {
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const data: any = {};
+    for (const name in inputs) {
+        data[name] = inputs[name].value
+    }
+    return data;
 }
